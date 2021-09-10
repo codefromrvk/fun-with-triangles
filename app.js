9dd0edcdbb1add1bbf04cd3d15621df33d6f71f0
codefromrvk <varshithkumar553@gmail.com>
2021-09-10 21:49:18 +0530

    complete basic functionality

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const angleInput = document.querySelectorAll(".angleInput")
 const buttonInput = document.querySelector("#btn-input")
-console.log(angleInput)
+const output = document.querySelector("#output")
+// console.log(angleInput)
 
 function checkTriangle() {
     let sum = 0;
     angleInput.forEach((ele) => {
         sum = sum + Number(ele.value);
     })
-    console.log(sum)
+    if (sum === 180) {
+        output.innerText = "Yes,it is a triangle";
+    }
+    else {
+        output.innerText = "Oh oh! it is not a triangle";
+    }
 }
 buttonInput.addEventListener("click", checkTriangle)
