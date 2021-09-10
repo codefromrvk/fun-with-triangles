a17b2ae5f92f021185f291066846b593071b93cc
codefromrvk <varshithkumar553@gmail.com>
2021-09-10 21:44:20 +0530

    access input elements

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+const angleInput = document.querySelectorAll(".angleInput")
+const buttonInput = document.querySelector("#btn-input")
+console.log(angleInput)
+
+function checkTriangle() {
+    let sum = 0;
+    angleInput.forEach((ele) => {
+        sum = sum + Number(ele.value);
+    })
+    console.log(sum)
+}
+buttonInput.addEventListener("click", checkTriangle)
